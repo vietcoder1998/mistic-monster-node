de2ff89a119522ec2665f83cbfe03955cd8349ae
viettd <duyviet2841998@gmail.com>
2021-10-16 19:59:34 +0700

    [viettd] recompiler

--- src/entities/Response.ts
+++ src/entities/Response.ts
@@ -1,12 +1,11 @@
-import { Message } from 'websocket'
 import { Code } from '../enums'
 
 export default class Response<T> {
     code: Code
-    msg?: Message
-    data: T | Message | string
+    msg?: string
+    data: T | string
 
-    constructor(code?: Code, data?: T | Message | string) {
+    constructor(code?: Code, data?: T | string) {
         if (data) {
             this.data = data
         }
