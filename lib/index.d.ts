f2d8bc71f44aca9115aaea8a1603d2b07ccc923f
viettd <duyviet2841998@gmail.com>
2021-10-16 19:55:10 +0700

    [viettd] recompile module

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 import { query, create_id, random_id, generate_stats, address } from './utils';
+import { BlockChain, Block, Node, Response, Account, Wallet } from './entities';
 import { ResponseMessage, RedisStore, MonsterType, AccountType, CoinUnit, TransactionType, Code, Message } from './enums';
 import { MonsterStats, MonsterAbility, SaleInfo, MonsterBodyPart, MonsterFigure, MonsterShortInfo, MonsterEntity, MonsterClass, BlockInfo, TransactionInfo, NodeInfo, Result, TransferHistory } from './typings';
-export { query, create_id, random_id, generate_stats, address, ResponseMessage, RedisStore, MonsterType, AccountType, CoinUnit, TransactionType, Code, Message, MonsterStats, MonsterAbility, SaleInfo, MonsterBodyPart, MonsterFigure, MonsterShortInfo, MonsterEntity, MonsterClass, BlockInfo, TransactionInfo, NodeInfo, Result, TransferHistory, };
+export { query, create_id, random_id, generate_stats, address, ResponseMessage, RedisStore, MonsterType, AccountType, CoinUnit, TransactionType, Code, Message, MonsterStats, MonsterAbility, SaleInfo, MonsterBodyPart, MonsterFigure, MonsterShortInfo, MonsterEntity, MonsterClass, BlockInfo, TransactionInfo, NodeInfo, Result, TransferHistory, BlockChain, Block, Node, Response, Account, Wallet, };
